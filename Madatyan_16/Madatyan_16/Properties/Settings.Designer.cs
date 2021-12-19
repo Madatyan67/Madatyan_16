c28f031461d08094b2f3214fa5f127078dd1c31a
Madatyan67 <96314593+Madatyan67@users.noreply.github.com>
2021-12-19 19:03:52 +0300

    Update Settings.Designer.cs

--- Madatyan_16/Madatyan_16/Properties/Settings.Designer.cs
+++ Madatyan_16/Madatyan_16/Properties/Settings.Designer.cs
@@ -9,7 +9,7 @@
 //------------------------------------------------------------------------------
 
 
-namespace Keshishyan16.Properties
+namespace Madatyan16.Properties
 {
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
